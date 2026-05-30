8c118330e1a6ece1e0b705a294b49b60756d7969
Douglas Panhota <douglaspands@gmail.com>
2026-05-30 02:54:06 -0300

    inclusao da quantidade de lançamentos mais elegante

--- app/infra/template.py
+++ app/infra/template.py
@@ -1,5 +1,3 @@
-from datetime import date
-
 from fastapi.templating import Jinja2Templates
 
 from app.infra import utils
@@ -7,8 +5,4 @@ from app.transaction.enum import CategoriaTransacaoEnum
 
 templates = Jinja2Templates(directory="templates")
 templates.env.filters["formatar_moeda"] = utils.formatar_moeda
-templates.env.filters["dia"] = lambda data: (
-    str(data.day) if isinstance(data, date) else ""
-)
-templates.env.filters["quantidade"] = lambda lista: len(lista)
 templates.env.globals["categorias_disponiveis"] = CategoriaTransacaoEnum  # type: ignore
