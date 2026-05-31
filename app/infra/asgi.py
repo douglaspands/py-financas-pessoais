b21752365773e5fe9cb013d5c6a5c57c3ca64bd2
Douglas Panhota <douglaspands@gmail.com>
2026-05-31 15:50:16 -0300

    dividindo templates no jinja2

--- app/infra/asgi.py
+++ app/infra/asgi.py
@@ -13,5 +13,9 @@ def create_app() -> FastAPI:
     app = FastAPI(lifespan=lifespan, doc_url=None, redoc_url=None)
     app.mount("/static", StaticFiles(directory="static"), name="static")
     app.include_router(expense_tracker_router)
-    app.get("/")(lambda: RedirectResponse(url="/gastos", status_code=302))
+    app.get("/")(
+        lambda: RedirectResponse(
+            url=expense_tracker_router.url_path_for("gastos:index"), status_code=302
+        )
+    )
     return app
